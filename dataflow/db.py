11f65697f38d22eb7efb9e07af5436ee86ed1863
Connor Burns <ctrekker4@gmail.com>
2020-08-23 21:46:59 -0600

    DB module updates and deletions

--- dataflow/db.py
+++ dataflow/db.py
@@ -118,18 +118,74 @@ class DatabaseQueryNode(BaseNode):
         self.declare_input('db')
 
         self.declare_output('rows', self.get_output__rows)
+        self.declare_output('query', self.get_output__query)
 
     def get_output__rows(self, env):
+        return self.resolve_input('db', env).query(self.get_output__query(env))
+
+    def get_output__query(self, env):
         conditions = []
         for n in range(self.condition_count):
             conditions.append(self.resolve_input(f'condition_{n}', env))
         db = self.resolve_input('db', env)
         db_object = self.resolve_input('object', env)
         db.define_object(db_object)
-        return db.query(DatabaseQuery(
+        return DatabaseQuery(
             db_object,
             conditions
-        ))
+        )
+
+
+class DatabaseInsertNode(BaseNode):
+    def __init__(self):
+        super().__init__()
+
+        self.declare_input('entry')
+        self.declare_input('object')
+        self.declare_input('db')
+        self.declare_output('insert', self.get_output__insert)
+
+    def get_output__insert(self, env):
+        entry = self.resolve_input('entry', env)
+        object_schema = self.resolve_input('object', env)
+        db = self.resolve_input('db', env)
+        db.define_object(object_schema)
+        return db.insert(object_schema, entry)
+
+
+class DatabaseUpdateNode(BaseNode):
+    def __init__(self):
+        super().__init__()
+
+        self.declare_input('object')
+        self.declare_input('query')
+        self.declare_input('entry')
+        self.declare_input('db')
+        self.declare_output('update', self.get_output__update)
+
+    def get_output__update(self, env):
+        object_schema = self.resolve_input('object', env)
+        query = self.resolve_input('query', env)
+        entry = self.resolve_input('entry', env)
+        db = self.resolve_input('db')
+        db.define_object(object_schema)
+        return db.update(object_schema, query, entry)
+
+
+class DatabaseDeleteNode(BaseNode):
+    def __init__(self):
+        super().__init__()
+
+        self.declare_input('object')
+        self.declare_input('query')
+        self.declare_input('db')
+        self.declare_output('delete', self.get_input__delete)
+
+    def get_input__delete(self, env):
+        object_schema = self.resolve_input('object', env)
+        query = self.resolve_input('query', env)
+        db = self.resolve_input('db')
+        return db.delete(object_schema, query)
 
 
 class DatabaseConditionNode(BaseNode):
@@ -156,6 +212,11 @@ class DatabaseConnectionNode(DataSourceNode):
         super().__init__(adapter)
 
 
+class SQLiteDatabaseConnectionNode(DatabaseConnectionNode):
+    def __init__(self, file):
+        super().__init__(SQLiteDatabaseAdapter(file))
+
+
 class DatabaseAdapter:
     def __init__(self):
         self.conn = None
@@ -176,10 +237,10 @@ class DatabaseAdapter:
     def insert(self, schema: ObjectSchema, object_entry):
         pass
 
-    def update(self, query_object, object_entry):
+    def update(self, schema: ObjectSchema, query_object: DatabaseQuery, object_entry: dict):
         pass
 
-    def delete(self, query_object):
+    def delete(self, schema: ObjectSchema, query_object: DatabaseQuery):
         pass
 
 
@@ -210,7 +271,7 @@ class SQLiteDatabaseAdapter(DatabaseAdapter):
         c = self.conn.cursor()
         where_clause = ''
         if len(query_object.conditions) > 0:
-            where_clause = ' WHERE ' + ' AND '.join([str(x.left) + x.op + str(x.right) for x in query_object.conditions])
+            where_clause = self._where_clause(query_object)
         query_sql = f'SELECT * FROM {query_object.schema.name}{where_clause}'
         c.execute(query_sql)
 
@@ -227,16 +288,27 @@ class SQLiteDatabaseAdapter(DatabaseAdapter):
         c = self.conn.cursor()
         insert_sql = f'INSERT INTO {schema.name}' \
                      f' ({",".join(object_entry.keys())}) VALUES ({",".join([self._to_sqlite_val(x) for x in object_entry.values()])}) '
-        print(insert_sql)
         c.execute(insert_sql)
         self.conn.commit()
         c.close()
 
-    def update(self, query_object, object_entry):
-        pass
+    def update(self, schema: ObjectSchema, query_object: DatabaseQuery, object_entry: dict):
+        c = self.conn.cursor()
 
-    def delete(self, query_object):
-        pass
+        entry_defs = ','.join([x[0] + ' = ' + self._to_sqlite_val(x[1]) for x in zip(object_entry.keys(), object_entry.values())])
+
+        update_sql = f'UPDATE {schema.name} SET {entry_defs} {self._where_clause(query_object)}'
+        c.execute(update_sql)
+        self.conn.commit()
+        c.close()
+
+    def delete(self, schema: ObjectSchema, query_object: DatabaseQuery):
+        c = self.conn.cursor()
+
+        delete_sql = f'DELETE FROM {schema.name} {self._where_clause(query_object)}'
+        c.execute(delete_sql)
+        self.conn.commit()
+        c.close()
 
     @staticmethod
     def _to_sqlite_type(field_type):
@@ -254,3 +326,17 @@ class SQLiteDatabaseAdapter(DatabaseAdapter):
         if t == str:
             return "'" + field_value.replace("'", "\'") + "'"
         return str(field_value)
+
+    @staticmethod
+    def _where_clause(query_object):
+        return ' WHERE ' + ' AND '.join([str(x.left) + x.op + SQLiteDatabaseAdapter._to_sqlite_val(x.right) for x in query_object.conditions])
+
+
+BaseNode.NodeRegistry.extend([
+    DatabaseObjectNode,
+    DatabaseObjectFieldNode,
+    DatabaseQueryNode,
+    DatabaseInsertNode,
+    DatabaseConnectionNode,
+    SQLiteDatabaseConnectionNode
+])
